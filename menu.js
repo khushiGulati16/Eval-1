399389a9bdc664bc5dc8719578f9ddc7f30722b9
Madhu2705 <156984446+Madhu2705@users.noreply.github.com>
2024-11-01 13:20:39 +0530

    .

--- menu.js
+++ menu.js
@@ -4,3 +4,19 @@ document.querySelectorAll('.add-to-cart').forEach(button => {
     alert(`Your item '${itemName}' has been added to the cart!`);
   });
 });
+
+function searchMenu() {
+  const searchInput = document.getElementById("searchInput").value.toLowerCase();
+  const menuItems = document.querySelectorAll(".menu-item");
+
+  menuItems.forEach((item) => {
+      const itemName = item.querySelector("h4").textContent.toLowerCase();
+      if (itemName.includes(searchInput)) {
+          item.style.display = "block";
+      } else {
+          item.style.display = "none";
+      }
+  });
+}
+
+
